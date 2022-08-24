2f846ba663f5af1acdd182c6621e319c87f9ac11
CosminBozai <cosminbozai@outlook.com>
2022-08-24 13:57:21 +0100

    Renamed "block" to "brick"

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 const tableController = (() => {
   const gameTable = document.getElementById("game-table");
-  function createBlocks(blocksNum) {
-    for (let i = 0; i < blocksNum; i++) {
-      let block = document.createElement("div");
-      block.classList.add("block");
-      gameTable.appendChild(block);
+  function createBricks(bricksNum) {
+    for (let i = 0; i < bricksNum; i++) {
+      let brick = document.createElement("div");
+      brick.classList.add("brick");
+      gameTable.appendChild(brick);
     }
     /* 
-    take how many blocks to make
+    take how many bricks to make
     make them through a loop  */
   }
 
-  function splitBlocks() {
-    let blocks = document.querySelectorAll(".block");
+  function splitBricks() {
+    let bricks = document.querySelectorAll(".brick");
     let leftPos = 0;
     let topPos = 0;
-    for (i = 0; i < blocks.length; i++) {
-      blocks[i].style.left = leftPos + "px";
-      blocks[i].style.top = topPos + "px";
+    for (i = 0; i < bricks.length; i++) {
+      bricks[i].style.left = leftPos + "px";
+      bricks[i].style.top = topPos + "px";
       leftPos += 102;
       if ((i + 1) % 8 === 0) {
         topPos += 22;
         leftPos = 0;
       }
     }
-    /* loop through the blocks and add positioning to them
+    /* loop through the bricks and add positioning to them
     if more than 8 just go to the next row*/
   }
-  return { createBlocks, splitBlocks };
+  return { createBricks, splitBricks };
 })();
